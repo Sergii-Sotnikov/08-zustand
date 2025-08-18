8ca8d6ba89f16fe10018f206a4b7a781b971fd32
SotnikovSS <sotnikovss90@gmail.com>
2025-08-18 23:18:03 +0200

    go

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,10 +1,36 @@
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "./NoteDetails.client";
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import { Metadata } from "next";
 
 type Props = {
   params: Promise<{ id: string }>;
 };
+
+export const generateMetadata = async ({ params }: Props):Promise<Metadata>=>{
+const {id} = await params;
+const data = await fetchNoteById(id);
+
+return {
+  title: data.title,
+  description: data.content.slice(0, 25),
+      openGraph: {
+      title: `NoteHub: ${data.title}`,
+      description: `${data.content.slice(0, 25)}`,
+      url: `https://08-zustand-eta-five.vercel.app/notes/${id}`,
+      siteName: `NoteHub: ${data.title}`,
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: `${data.title}`,
+        },
+      ],
+      type: 'website',
+    },
+}
+}
 
 const NoteDetails = async ({ params }: Props) => {
   const { id } = await params;
