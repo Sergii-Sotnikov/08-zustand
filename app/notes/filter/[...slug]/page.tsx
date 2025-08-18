a9e315a0948b355f0cfcb18d08d194d285e10980
SotnikovSS <sotnikovss90@gmail.com>
2025-08-18 22:05:52 +0200

    updt

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,10 +1,20 @@
 
 import {fetchNotes} from "@/lib/api"
 import NotesClients from "./Notes.client";
+import type { Metadata } from "next";
 
 
 type Props = {
   params: Promise<{slug: string[]}>;
+}
+
+export const generateMetadata = async ({params}:Props): Promise<Metadata> =>{
+  const {slug} = await params
+  console.log(slug[0])
+  return {
+    title: `NoteHub: ${slug[0]}`,
+    description: `Browse ${slug[0]} notes in NoteHub. Create, edit and search your ${slug[0]} notes with a clean, fast interface.`,
+  }
 }
 
 export default async function Notes({params}:Props){
